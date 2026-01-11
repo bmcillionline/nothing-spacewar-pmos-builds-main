c3af8d6194896854aad7e6dad6f0ef661854e6f7
bmcillionline <91141553+bmcillionline@users.noreply.github.com>
2026-01-11 00:09:39 -0500

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,62 +1,69 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 set -e
 
-# Clean up first
-sudo rm -rf ~/.local/var/pmbootstrap
-rm -rf out
-rm -rf pmbootstrap
+export ARCH=arm64
+export CROSS_COMPILE=aarch64-linux-gnu-
+export dir=/tmp
 
-# Git identity
-git config --global user.email "bmcillionline@hotmail.com"
-git config --global user.name "bmcillionline"
+if [ -n "$ACT" ]; then
+  echo "127.0.0.1 fedora" | sudo tee -a /etc/hosts
+fi
 
-# Replace placeholders in .cfg file
-find . -type f -name "*.cfg" -exec sed -i "s|HOME|$(echo $HOME)|;s|NPROC|$(nproc)|" {} +
+if command -v dnf; then
+  sudo dnf upgrade -y
+  sudo dnf install -y python3-pip bc make kpartx flex bison gcc gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu openssl-devel
+elif command -v apt; then
+  sudo add-apt-repository -y universe
+  sudo apt upgrade -y
+  sudo apt install -y python3-pip bc make kpartx flex bison gcc gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu libssl-dev
+  python3 -m pip install uv
+  python3 -m uv python install 3.13
+  python3 -m uv python pin 3.13
+  export PATH=$(echo ~/.local/share/uv/python/cpython-3.13.*/bin):$PATH
+fi
 
-# Setup environment
-export KERNEL_BRANCH=danila/spacewar-testing
+if [ ! -d $dir/linux ]; then
+  git clone https://github.com/mainlining/linux --branch danila/spacewar-testing $dir/linux --depth 1
+fi
+if [ ! -d $dir/pmbootstrap ]; then
+  git clone https://gitlab.postmarketos.org/postmarketOS/pmbootstrap $dir/pmbootstrap 
+fi
+if [ ! -d $dir/pmaports ]; then
+  git config --global user.email "veythrix@gmail.com"
+  git config --global user.name "Veythrix"
+  git clone https://gitlab.postmarketos.org/postmarketOS/pmaports.git $dir/pmaports
+  cd $dir/pmaports
+  git remote add mainlining https://github.com/mainlining/pmaports
+  git fetch mainlining danila/spacewar
+  git merge mainlining/danila/spacewar -X theirs -m merge
+fi
 
-# Install pmbootstrap from Git
-git clone https://gitlab.postmarketos.org/postmarketOS/pmbootstrap.git --depth 1
-mkdir -p ~/.local/bin
-export PATH="$PATH:~/.local/bin"
-sudo rm /home/ubuntu/.local/bin/pmbootstrap
-ln -s "$PWD/pmbootstrap/pmbootstrap.py" ~/.local/bin/pmbootstrap
-pmbootstrap --version
+mkdir ~/.config || true
+echo """
+[pmbootstrap]
+aports = $dir/pmaports
+device = nothing-spacewar
+ui = phosh
+systemd = never
+""" | tee ~/.config/pmbootstrap_v3.cfg
 
-# Init
-echo -e '\n\n' | pmbootstrap init || true
-cd ~/.local/var/pmbootstrap/cache_git/pmaports
-
-# Kernel branch setup
-export DEFAULT_BRANCH=danila/spacewar-mr
-git remote add sc7280 https://github.com/mainlining/pmaports.git
-git fetch sc7280 $DEFAULT_BRANCH
-git reset --hard sc7280/$DEFAULT_BRANCH
-export DEFAULT_BRANCH=danila/spacewar-testing
-echo "Default branch is $DEFAULT_BRANCH"
-git clone https://github.com/mainlining/linux.git --single-branch --branch $KERNEL_BRANCH --depth 1
-
-# Copy config to pmbootstrap
-cp /home/ubuntu/pmos/nothing-spacewar.cfg ~/.config/pmbootstrap_v3.cfg
-
-# Compile kernel image
-cd linux
+cd $dir/linux
+yes '' | python3 $dir/pmbootstrap/pmbootstrap.py init
 shopt -s expand_aliases
-source /home/ubuntu/pmos/pmbootstrap/helpers/envkernel.sh
+source $dir/pmbootstrap/helpers/envkernel.sh
 make defconfig sc7280.config
 make -j$(nproc)
-pmbootstrap build --envkernel linux-postmarketos-qcom-sc7280
+pmbootstrap build linux-postmarketos-qcom-sc7280 --force --envkernel
+pmbootstrap install --password 7777 
+if [ -n "$GITHUB_WORKSPACE" ]; then
+  pmbootstrap shutdown
+fi
 
-# Build pmos images
-cp /home/ubuntu/pmos/nothing-spacewar.cfg ~/.config/pmbootstrap.cfg
-pmbootstrap install --password 1114
+mkdir $dir/artifacts || true
+mkdir $dir/artifacts-compressed || true
+cp ~/.local/var/pmbootstrap/chroot_rootfs_nothing-spacewar/boot/boot.img $dir/artifacts/nothing-spacewar-boot.img
+cp ~/.local/var/pmbootstrap/chroot_native/home/pmos/rootfs/nothing-spacewar.img $dir/artifacts/nothing-spacewar.img
+xz -c $dir/artifacts/nothing-spacewar-boot.img > $dir/artifacts-compressed/nothing-spacewar-boot.img.xz
+xz -c $dir/artifacts/nothing-spacewar.img > $dir/artifacts-compressed/nothing-spacewar.img.xz
 
-# Export build images to outdir
-pmbootstrap export
-mkdir /home/ubuntu/pmos/out
-
-cp /tmp/postmarketOS-export/boot.img /home/ubuntu/pmos/out/boot-nothing-spacewar.img
-cp /tmp/postmarketOS-export/nothing-spacewar.img /home/ubuntu/pmos/out/rootfs-nothing-spacewar.img
-tar -c -I 'xz -9 -T0' -f /home/ubuntu/pmos/out/Spacewar_pmos.tar.xz /home/ubuntu/pmos/out/rootfs-nothing-spacewar.img /home/ubuntu/pmos/out/boot-nothing-spacewar.img
-echo -e "n\nn\ny\n" | pmbootstrap zap
